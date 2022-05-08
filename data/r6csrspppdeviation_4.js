298130b68026437aedf26dafd4c15fa5bc363077
Team-E-HCU <105145349+Team-E-HCU@users.noreply.github.com>
2022-05-08 09:11:45 +0200

    Add files via upload

---
+++ data/r6csrspppdeviation_4.js
@@ -0,0 +1,8 @@
+var json_r6csrspppdeviation_4 = {
+"type": "FeatureCollection",
+"name": "r6csrspppdeviation_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "X": null, "Y": null, "sdlat": null, "sdlon": null, "sdne": null, "ns": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } }
+]
+}
